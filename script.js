a09e58f3c18ad33ceadd27e91f9b8b28de1246b6
Isaac Watts <Isaac.s.watts@gmail.com>
2023-08-08 11:18:24 -0400

    Changed div ids to be session ids instead

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function opentab(tabName) {
     for (i = 0; i < x.length; i++) {
       x[i].style.display = "none";  
     }
+    
     document.getElementById(tabName).style.display = "block";  
   }
+
 function openNav() {
   document.getElementById("mySidebar").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
